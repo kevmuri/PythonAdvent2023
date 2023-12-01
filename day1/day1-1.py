7976592e0645398cfb08c81806ffe97b616e096b
kevmuri <kevmuri@umich.edu>
2023-12-01 12:21:59 -0500

    Completion of 1-1

--- day1/day1-1.py
+++ day1/day1-1.py
@@ -1,1 +1,15 @@
-print('hello world')
+inputFile = open("day1-1-input.txt", "r")
+lines = inputFile.readlines()
+
+count = 0
+for line in lines:
+    digits = []
+    for i in line:
+        if i.isnumeric():
+            digits.append(i)
+    firstDigit = digits[0]
+    lastDigit = digits[-1]
+    numberToAdd = int(str(firstDigit) + str(lastDigit))
+    count += numberToAdd
+
+print(count)

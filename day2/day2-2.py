4410c5117500205ce4f1eb63a14c8f56d2d9411c
kevmuri <72419314+kevmuri@users.noreply.github.com>
2023-12-02 13:06:36 -0500

    Completed 2-2

---
+++ day2/day2-2.py
@@ -0,0 +1,44 @@
+# 12 red
+# 13 green
+# 14 blue
+def process_input():
+    input_file = open("day2-input.txt", "r")
+    lines = input_file.readlines()
+
+    input_array = []
+
+    for line in lines:
+        line = line.split(":")
+        hands = line[1].split(";")
+        # Each game is an array of hands
+        hand_array = []
+        for hand in hands:
+            cube_counts = hand.split(",")
+            # A hand is a dictionary with colors and their counts
+            cube_count_object = {"red": 0, "green": 0, "blue": 0}
+            for cube_count in cube_counts:
+                cube_count = cube_count.replace("\n", "")
+                cube_count = cube_count.replace(" ", "", 1)
+                cube_count_split = cube_count.split(" ")
+                cube_count_object[cube_count_split[1]] = cube_count_split[0]
+            hand_array.append(cube_count_object)
+        input_array.append(hand_array)
+
+    return input_array
+
+
+def main():
+    games = process_input()
+    added_power = 0
+    for game in games:
+        minimums = {"red": 0, "green": 0, "blue": 0}
+        for hand in game:
+            for color in hand.keys():
+                if int(hand[color]) > minimums[color]:
+                    minimums[color] = int(hand[color])
+        added_power += minimums["red"] * minimums["green"] * minimums["blue"]
+
+    print(added_power)
+
+
+main()
